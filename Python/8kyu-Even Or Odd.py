34d13c34b6526a1a1050948c1ac2fb977aba6583
jangoertzen <58172201+jangoertzen@users.noreply.github.com>
2020-03-11 15:35:27 +0100

    2 added

---
+++ Python/8kyu-Even Or Odd.py
@@ -0,0 +1,4 @@
+#Create a function(or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(n):
+  return 'Even' if n % 2 == 0 else 'Odd'
